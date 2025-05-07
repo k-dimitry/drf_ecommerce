ea51b705cecf36d8b5ec24c000128a335146974f
Dimitry <tizya.noonie@gmail.com>
2025-05-07 19:21:11 +0300

    In the process of step 4.10.5 at stepik.org

--- apps/sellers/urls.py
+++ apps/sellers/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.sellers.views import SellersView
+from apps.sellers.views import SellersView, SellerProductsView
 
 urlpatterns = [
     path('', SellersView.as_view()),
+    path('products/', SellerProductsView.as_view()),
 ]
--- apps/sellers/views.py
+++ apps/sellers/views.py
@@ -4,6 +4,8 @@ from rest_framework.views import APIView
 
 from apps.sellers.models import Seller
 from apps.sellers.serializers import SellerSerializer
+from apps.shop.models import Category, Product
+from apps.shop.serializers import ProductSerializer, CreateProductSerializer
 
 tags = ['Sellers']
 
@@ -29,3 +31,51 @@ class SellersView(APIView):
             return Response(data=serializer.data, status=201)
         else:
             return Response(data=serializer.errors, status=400)
+
+
+class SellerProductsView(APIView):
+    serializer_class = ProductSerializer
+
+    @extend_schema(
+        summary="Seller Products Fetch",
+        description="""
+            This endpoint returns all products from a seller.
+            Products can be filtered by name, sizes or colors.
+        """,
+        tags=tags,
+    )
+    def get(self, request, *args, **kwargs):
+        seller = Seller.objects.get_or_none(user=request.user, is_approved=True)
+        if not seller:
+            return Response(data={"message": "Access is denied"}, status=403)
+        products = Product.objects.select_related("category", "seller", "seller__user").filter(seller=seller)
+        serializer = self.serializer_class(products, many=True)
+        return Response(data=serializer.data, status=200)
+
+    @extend_schema(
+        summary="Create a product",
+        description="""
+            This endpoint allows a seller to create a product.
+        """,
+        tags=tags,
+        request=CreateProductSerializer,
+        responses=CreateProductSerializer,
+    )
+    def post(self, request, *args, **kwargs):
+        serializer = CreateProductSerializer(data=request.data)
+        seller = Seller.objects.get_or_none(user=request.user, is_approved=True)
+        if not seller:
+            return Response(data={"message": "Access is denied"}, status=403)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            category_slug = data.pop("category_slug", None)
+            category = Category.objects.get_or_none(slug=category_slug)
+            if not category:
+                return Response(data={"message": "Category does not exist!"}, status=404)
+            data['category'] = category
+            data['seller'] = seller
+            new_prod = Product.objects.create(**data)
+            serializer = self.serializer_class(new_prod)
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
